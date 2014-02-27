7a59a00d00b455dfad2c9ef6efa99e1c2b7bd4f7
moohoorama <yw0119.kim@samsung.com>
2014-02-27 18:28:44 +0900

    add template rcuref.h

---
+++ include/ywrcuref.h
@@ -0,0 +1,14 @@
+/* Copyright [2014] moohoorama@gmail.com Kim.Youn-woo */
+
+#ifndef INCLUDE_YWRCUREF_H_
+#define INCLUDE_YWRCUREF_H_
+
+#include <ywcommon.h>
+#include <ywthread.h>
+#include <ywspinlock.h>
+#include <ywsll.h>
+
+template<typename T, bool use_atomic = false>
+class ywRCUREF{
+
+#endif  // INCLUDE_YWRCUREF_H_
